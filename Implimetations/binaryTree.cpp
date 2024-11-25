5cafe16d8d51baf922785c0a20f1aca4c95d0cb6
Ala-Eddine-Mezdoud <mezdoud.ala.e@gmail.com>
2024-11-25 12:12:35 +0100

    trees implimentation added

--- Implimetations/binaryTree.cpp
+++ Implimetations/binaryTree.cpp
@@ -21,4 +21,12 @@ public:
     {
         root->data = value;
     };
+    int countNodes()
+    {
+        if (root = NULL)
+        {
+            return 0;
+        }
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
 };
