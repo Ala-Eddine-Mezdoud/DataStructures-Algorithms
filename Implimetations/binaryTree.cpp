0b81a0da021fa74c837e5b03d5e700efafdaec4b
Ala-Eddine-Mezdoud <mezdoud.ala.e@gmail.com>
2024-11-25 12:05:43 +0100

    trees implimentation added

--- Implimetations/binaryTree.cpp
+++ Implimetations/binaryTree.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 template <typename Object>
-struct binaryTree
+class binaryTree
 {
+private:
     Object data;
     binaryTree *left;
     binaryTree *right;
+
+public:
     binaryTree(Object value) : data(value), left(nullptr), right(nullptr) {}
 };
